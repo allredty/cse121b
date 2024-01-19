a5116b7fd91b570c14853555732377e0c250d611
allredty <all21059@byui.edu>
2024-01-19 10:20:56 -0700

    updated js code.

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -7,20 +7,28 @@ const fullName = "Tyler Allred"
 const Year = new Date();
 let currentYear = Year.getFullYear();
 let profilePicture = ("images/me.jpg");
+
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
-const imageElement = document.querySelector("profileImage");
+const imageElement = document.querySelector(`img`);
+
 /* Step 4 - Adding Content */
-imageElement.innerHTML = fullName;
+/*imageElement.innerHTML = fullName;
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
 imageElement.setAttribute("src", profilePicture);
 imageElement.setAttribute("alt", `Profile image of ${fullName}`);
+*/
+imageElement.setAttribute("src", `${profilePicture}`);
+nameElement.innerHTML = (`<strong>${fullName}</strong>`)
+yearElement.innerHTML = (`${currentYear}`)
+imageElement.setAttribute("alt", `profile image of ${fullName}`);
+
 /* Step 5 - Array */
-const fav_food = ["Enchiladas", "Taco Bell", "Smoothies"];
-const oneMoreFood = "Pizza";
+let fav_food = ["Enchiladas", "Taco Bell", "Smoothies"];
+let oneMoreFood = "Pizza";
 fav_food.push(oneMoreFood);
 foodElement.innerHTML += `<br>${fav_food}`;
 fav_food.shift();
