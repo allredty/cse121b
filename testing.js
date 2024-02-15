ed21aff17e23a077d50f89991765a1c75c5eda78
allredty <all21059@byui.edu>
2024-02-14 18:43:30 -0700

    did a thing

--- testing.js
+++ testing.js
@@ -1,9 +1,4 @@
-let names = ['Nancy','Blessing','Jorge','Svetlana','Bob'];
-
-const namesB = names.filter(name => name.charAt(0) === 'B');
-
-let namesLength = [5, 8, 5, 8, 3];
-
-let nameLengths = names.map((name) => name.length);
-
-names.reduce((total, name) => total + name.length, 0) / names.length;
+function multiply(num1, num2){
+    let result = num1 * num2;
+    return result;
+}
